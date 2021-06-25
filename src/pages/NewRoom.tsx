0bb8ff5c9f016a336d793fbbd692ff5dcc26903d
Alex Solda <alexsoldaa@gmail.com>
2021-06-25 16:47:33 -0300

    Contexto de autenticação refatorado em um componente separado

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,17 +1,15 @@
 import {Link} from 'react-router-dom';
 
-import { useContext } from 'react';
-import { AuthContext } from '../App';
-
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg'
 import '../styles/auth.scss';
 
 import {Button} from '../components/Button';
+// import { useAuth } from '../hooks/useAuth';
 
 export function NewRoom() {
 
-    const {user} = useContext(AuthContext);
+    // const {user} = useAuth();
 
     return (
         <div id='page-auth'>
@@ -23,7 +21,6 @@ export function NewRoom() {
             <main>
                 <div className='main-content'>
                     <img src={logoImg} alt='Logo escrito let me ask com um balão de conversa em volta' />
-                    <h3>{user?.name}</h3>
                     <h2>Criar uma nova sala</h2>
                     <form>
                         <input
